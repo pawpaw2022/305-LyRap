e9f4849d18645982699d29570c89f18e88ce12e8
gresli <59580281+gresli@users.noreply.github.com>
2022-04-18 16:50:43 -0700

    Renamed the file to capital S for SearchBox

--- searchBox.py
+++ searchBox.py
@@ -19,21 +19,21 @@ SearchBox is a superclass that contains:
 from abc import abstractmethod
 import json_test
 
+
 class SearchBox(object):
-    def __init__(self, name:str) -> None:
+    def __init__(self, name: str) -> None:
         self.name = name
         self.result = []
-        self.criteria = "DEFAULT" # title, lyrics, artist.
-        
+        self.criteria = "DEFAULT"  # title, lyrics, artist.
+
     @abstractmethod
-    def search(self, keyword:str) -> list: 
-        for song in json_test.song_db: 
+    def search(self, keyword: str) -> list:
+        for song in json_test.song_db:
             if keyword in song[self.criteria]:
                 self.result.append(f"{song['title']} - {song['artist']}")
                 return "Result has been found !!!"
-    
+
         return "No result matched."
-        
 
     def getResult(self) -> list:
         return self.result
@@ -41,9 +41,8 @@ class SearchBox(object):
     def getName(self) -> str:
         return self.name
 
-    def setName(self,name) -> None:
+    def setName(self, name) -> None:
         self.name = name
 
     def __str__(self) -> str:
         return f" This is a {self.criteria} search Box, its name is {self.getName()}, currently it has found {len(self.result)} results."
-
