4987dfa9f30a0fc3ece28ddc9692b1551b9a158e
paul <paul.l.sining@gmail.com>
2022-05-26 10:41:44 -0700

    add description to addsong.py

--- AddSong.py
+++ AddSong.py
@@ -1,5 +1,7 @@
 """
-DESCRIPTION:
+DESCRIPTION: ADDSONG is a subclass of SearchBox and allows
+the user to add a song using user entered atrist, song title and song.
+The results will produce the a new songLibrary the contains all new user added songs in a seperate txt file.
 
 AUTHORS: Grace Gresli
 
@@ -22,35 +24,39 @@ import lyricsgenius as lg
 
 
 class AddSong(object):
-    
+
     genius_access_token = "_rDAy259SoA7baRL1ouy2MWdOADsdevMvTvMxo83XXtJJ-ejmfSNvtZSrbqJZrvZ"
     genius_object = lg.Genius(genius_access_token)
 
-    def __init__(self, title:str, artist:str, lyrics:str) -> None:
+    def __init__(self, title: str, artist: str, lyrics: str) -> None:
         self.title = title
         self.lyrics = lyrics
         self.artist = artist
+
     def getTitle(self):
         return self.title
+
     def getLyrics(self):
         return self.lyrics
+
     def getArtist(self):
         return self.artist
-    def addSong(self):
 
+    def addSong(self):
 
         try:
             filename = "newSongLibrary.txt"
             with open(filename, "w") as f:
                 while True:
                     try:
-                        f.write("Song Title: " + self.title + "\n" + "Song Artist: " + self.artist + "\n" + "Song Lyrics: "+ self.lyrics + "\n")
+                        f.write("Song Title: " + self.title + "\n" + "Song Artist: " +
+                                self.artist + "\n" + "Song Lyrics: " + self.lyrics + "\n")
                         f.write("\n")
-                        f.close() #with and as closes the file autamatically
+                        f.close()  # with and as closes the file autamatically
                     except EOFError:
                         break
         except:
-            print("Song Successfuly Added!") 
+            print("Song Successfuly Added!")
 
     # override
     def __str__(self):
